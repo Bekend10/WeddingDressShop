2011a3b2f42060bd04e1cd6762e0d3a9da254e9c
Nguyen Quang Duy <nguyenquangduy12112004@gmail.com>
2024-12-21 09:28:59 +0700

    cart

--- E_WeddingDressShop/Controllers/CartController.cs
+++ E_WeddingDressShop/Controllers/CartController.cs
@@ -17,13 +17,50 @@ namespace E_WeddingDressShop.Controllers
             conn = new SqlConnection(SqlCon);
         }
 
-        private void AddCart(SqlCommand cmd, CART cart)
+        private string AddCart(CART cart)
         {
-            
+            try
+            {
+                string sql = "INSERT INTO tb_Carts (CartID, UserID, ProductID, Quantity) " +
+                             "VALUES (@CartID, @UserID, @ProductID, @Quantity, @ImageUrl)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                AddCartParamater(cmd, cart);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Thêm giỏ hàng thành công!";
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                return "Lỗi khi thêm giỏ hàng: " + ex.Message;
+            }
         }
-        private void RemoveItem(SqlCommand cmd, CART cart)
+        private string DeleteCart(int CartID)
         {
+            try
+            {
+                string sql = "DELETE FROM tb_Carts WHERE CartID = @CartID";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@CartID", CartID);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return "Xóa giỏ hàng thành công!";
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                return "Lỗi khi xóa giỏ hàng: " + ex.Message;
+            }
+        }
 
+        private void AddCartParamater(SqlCommand cmd, CART cart)
+        {
+            cmd.Parameters.AddWithValue("@CartID", cart.CartID);
+            cmd.Parameters.AddWithValue("@UserID", cart.UserID);
+            cmd.Parameters.AddWithValue("@ProductID", cart.ProductID);
+            cmd.Parameters.AddWithValue("@Quantity", cart.Quantity);
         }
     }
 }
