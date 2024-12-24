788dd9d7d58e980db8427adb2dcc2dea33ba17ed
DuongVQ <duongvq392@gmail.com>
2024-12-25 02:37:01 +0700

    add css btn

--- E_WeddingDressShop/Views/Clients/Register.aspx.designer.cs
+++ E_WeddingDressShop/Views/Clients/Register.aspx.designer.cs
@@ -87,6 +87,15 @@ namespace E_WeddingDressShop.Views
         protected global::System.Web.UI.WebControls.TextBox txtAddress;
 
         /// <summary>
+        /// btnLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
+
+        /// <summary>
         /// btnRegister control.
         /// </summary>
         /// <remarks>
